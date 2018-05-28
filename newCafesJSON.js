d8a739db9210b6a4dbcbf7437965514a9f0645e5
MAS <digitalcolony@gmail.com>
2018-05-28 16:26:46 -0700

    newCafe job send email alert

--- newCafesJSON.js
+++ newCafesJSON.js
@@ -1,6 +1,7 @@
 const yelp = require("./lib/yelp-api");
 const Client = require("ftp");
 const fs = require("fs");
+const nodemailer = require("nodemailer");
 require("./config/config");
 
 const search = {
@@ -34,9 +35,38 @@ const uploadNewCafeData = async () => {
   });
 };
 
+const emailMichael = async () => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env["EMAIL"],
+      pass: process.env["EMAIL_PASSWORD"]
+      // GMAIL TIP: create device password @ https://myaccount.google.com/security --> App Passwords
+    }
+  });
+
+  const mailOptions = {
+    from: process.env["EMAIL"],
+    to: process.env["EMAIL"],
+    subject: "Cafe Informant",
+    text: "Job executed!"
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
+
 const runReport = async () => {
   const goYelp = await getNewCafeData();
   const goFTP = await uploadNewCafeData();
+  const goEmail = await emailMichael();
 };
 
 runReport();
